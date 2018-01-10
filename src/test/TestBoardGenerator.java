654cfb363716fce339830a1770ed7c2fb85f9144
Nathaniel Corley <nscorley@gmail.com>
2018-01-09 23:05:11 -0800

    Basic test structure, and HCN tests.

---
+++ src/test/TestBoardGenerator.java
@@ -0,0 +1,50 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import main.Utils;
+
+public class TestBoardGenerator {
+	
+	/**
+	 * Randomly generate boards from the Utils.generateRandomBoard() function, and saves to file.
+	 * Does not need to be run, unless more positions are wanted.
+	 * @param args: number of random boards to generate
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		
+		// determine number of times
+		int times = args.length > 0 ? Integer.parseInt(args[0]) : 50;
+		
+		// connect to the file
+		try(FileWriter fw = new FileWriter("src/test/random-positions.txt", true);
+			    BufferedWriter bw = new BufferedWriter(fw);
+			    PrintWriter out = new PrintWriter(bw))
+			{
+			 	for(int i = 0; i < times; i++) {
+			 		String s = Utils.boardToHCN(Utils.generateRandomBoard());
+			 		out.println(s);
+			    }
+			} catch (IOException e) {
+			    // throw exception
+				throw new RuntimeException("Error writing to random-positions.txt file.");
+			}
+		
+		// count number of lines
+		BufferedReader reader = new BufferedReader(new FileReader("src/test/random-positions.txt"));
+		int lines = 0;
+		while (reader.readLine() != null) lines++;
+		reader.close();
+		
+		// alert user
+		System.out.println("Wrote " + times + " new positions to random-positions.txt file.");
+		System.out.println("Test file now contains " + lines + " generated test positions.");
+	}
+
+}
---
+++ src/test/TestHCN.java
@@ -0,0 +1,46 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import main.Board;
+import main.Utils;
+
+/**
+ * Test class to confirm that HCN functions are working as expected. HCN
+ * functions include: boardToHCN(), subBoardToHCN(), and Board's HCN
+ * constructor.
+ */
+class TestHCN {
+
+	/**
+	 * We create a board from HCN, convert that board to an HCN, and assert they are
+	 * the same.
+	 * 
+	 * @throws IOException
+	 */
+	@DisplayName("Board From HCN and Back")
+	@Test
+	void boardFromHCNAndBack() throws IOException {
+		int count = 0;
+		String[] files = new String[] { "general-positions.txt", "random-positions.txt" };
+		for (String s : files) {
+			File f = new File("src/test/" + s);
+			BufferedReader br = new BufferedReader(new FileReader(f));
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				Assertions.assertEquals(Utils.boardToHCN(new Board(line)), line,
+						"Incorrect result in boardFromHCNAndBack().");
+				count++;
+			}
+			br.close();
+		}
+		System.out.println("Completed " + count + " positions.");
+	}
+}
