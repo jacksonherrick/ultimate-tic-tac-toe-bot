279dd1d376875478d5c5c3c9b126bf05320230cd
occook2 <119069061+occook2@users.noreply.github.com>
2023-07-25 21:32:55 -0400

    Move refactor, target subBoard + toString

--- src/main/java/com/jherrick/Move.java
+++ src/main/java/com/jherrick/Move.java
@@ -1,23 +1,33 @@
 package com.jherrick;
 
 public class Move{
+  
+  // =============== Instance Variables =================
   public int move;
   public int board;
 
+
+
+  // ==========-======= Constructors ====================
   public Move(int _move, int _board){
   	move = _move;
     board = _board;
   }
 
+
+
+  // ================ Public Functions ==================
+
   // Gets the SubBoard that we are sending the next player to based on the last move. Can make this more efficient - make a hashMap of translations
   public int getSubBoardTarget(){
     for(int i=0; i < Constants.BIT_MASKS.length; i++){
-      if((move & Constants.BIT_MASKS[i]) == Constants.BIT_MASKS[i])
+      if(isSubBoardTargetNumber(i))
         return i;
     }
     return -1;
   }
 
+  
   /**
     * @override the toString() method
     * converts integer move into readable representation
@@ -27,15 +37,15 @@ public class Move{
     StringBuilder result = new StringBuilder();
 
     // calculate row and column
-    int index = Utils.lowestSetBit(move);
-    int row = (2 - board / 3) * 3 + (index / 3) + 1;
-    int col = (board % 3) * 3 + (2 - index % 3) + 97;
+    int[] rowCol = calculateRowandColforString();
 
     // print basic information
-    result.append(Character.toString((char)col) + row);
+    result.append(Character.toString((char)rowCol[1]) + rowCol[0]);
     return result.toString();
   }
 
+
+  
   @Override
   public boolean equals(Object otherObject){
     // null check
@@ -55,5 +65,27 @@ public class Move{
     // field comparison
     return this.move == otherMove.move && this.board == otherMove.board;
 
+  }
+
+
+
+  // ================ Helper Functions ===================
+
+
+  // ======== Get SubBoard Target Helper Functions =======
+  private boolean isSubBoardTargetNumber(int possibleSubBoard){
+    return (move & Constants.BIT_MASKS[possibleSubBoard]) == Constants.BIT_MASKS[possibleSubBoard];
+  }
+
+
+  // ============ To String Helper Functions =============
+
+  private int[] calculateRowandColforString(){
+    int index = Utils.lowestSetBit(move);
+    int row = (2 - board / 3) * 3 + (index / 3) + 1;
+    int col = (board % 3) * 3 + (2 - index % 3) + 97;
+
+    int[] rowCol = new int[] {row, col};
+    return rowCol;
   }
 }
